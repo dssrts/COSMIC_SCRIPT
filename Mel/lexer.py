a657e38d0e6e3e7a1f673db088f2e37e17574d0d
encyklopedia <149690701+seegyulater@users.noreply.github.com>
2023-11-08 08:49:58 +0000

    edited saturn, shift, star, takeoff, trace, true

--- Mel/lexer.py
+++ Mel/lexer.py
@@ -2,6 +2,7 @@
 all_num = '0123456789'
 all_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphanum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+space_delim = " "
 
 #errors
 error = []
@@ -56,13 +57,15 @@ IDENTIFIER = 'IDENTI'
 COMMA = 'COMMA'
 SPACE = 'SPACE'
 
+
+
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
     
     def __repr__(self):
-        if self.value: return f'{self.type}: {self.value}'
+        if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
 
@@ -78,7 +81,8 @@ class Lexer:
 
     def advance(self):
         self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        # current char is the current pos if the pos is less than the length of the text
+        self.current_char = self.text[self.pos] if self.pos <= len(self.text)-1 else None
 
     def make_tokens(self):
         tokens = []
@@ -129,7 +133,7 @@ class Lexer:
         if errors:
             return errors
         else:
-            return tokens      
+            return tokens       
 
     def make_number(self):
         num_str = ''
@@ -157,11 +161,13 @@ class Lexer:
             return Token(INTEL, int(num_str))
         else:
             return Token(GRAVITY, float(num_str))
+        
+    
     def make_word(self):
         ident = ""
         
         while self.current_char != None and self.current_char in alphanum:
-            if self.current_char == "b": #bang, blast
+            if self.current_char == "b":
                 ident += self.current_char
                 self.advance()
                 if self.current_char == "a":
@@ -173,7 +179,19 @@ class Lexer:
                         if self.current_char == "g":
                             ident += self.current_char
                             self.advance()
-                            return Token(BANG, "bang")
+                            # catch if bang lang yung tinype ng user
+                            if self.current_char == None:
+                                return Token(BANG, "bang")
+                            #delimiter ng bang defined in space_delim
+                            if self.current_char not in space_delim:
+                                while self.current_char in alphanum and self.current_char not in space_delim:
+                                    ident += self.current_char
+                                    self.advance()
+                                    if self.current_char == None:
+                                        return Token(IDENTIFIER, ident)
+                                ###test
+                            else:
+                                return Token(BANG, "bang")
                 elif self.current_char == "l":
                     ident += self.current_char
                     self.advance()
@@ -367,7 +385,91 @@ class Lexer:
                             if self.current_char == "r":
                                 ident += self.current_char
                                 self.advance()
-                                return Token(OUTER, "outer")                          
+                                return Token(OUTER, "outer")
+            if self.current_char == "s": #saturn, shift, star
+                ident += self.current_char
+                self.advance()
+                if self.current_char == "a":
+                    ident += self.current_char
+                    self.advance()
+                    if self.current_char == "t":
+                        ident += self.current_char
+                        self.advance()
+                        if self.current_char == "u":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "r":
+                                ident += self.current_char
+                                self.advance()
+                                if self.current_char == "n":
+                                    ident += self.current_char
+                                    self.advance()
+                                    return Token(SATURN, "saturn")
+                elif self.current_char == "h":
+                        ident += self.current_char
+                        self.advance()
+                        if self.current_char == "i":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "f":
+                                ident += self.current_char
+                                self.advance()
+                                if self.current_char == "t":
+                                    ident += self.current_char
+                                    self.advance()
+                                    return Token(SHIFT, "shift")
+                elif self.current_char == "t":
+                        ident += self.current_char
+                        self.advance()
+                        if self.current_char == "a":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "r":
+                                ident += self.current_char
+                                self.advance()
+                                return Token(STAR, "star")
+            if self.current_char == "t": #takeoff, trace, true
+                ident += self.current_char
+                self.advance()
+                if self.current_char == "a":
+                    ident += self.current_char
+                    self.advance()
+                    if self.current_char == "k":
+                        ident += self.current_char
+                        self.advance()
+                        if self.current_char == "e":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "o":
+                                ident += self.current_char
+                                self.advance()
+                                if self.current_char == "f":
+                                    ident += self.current_char
+                                    self.advance()
+                                    if self.current_char == "f":
+                                        ident += self.current_char
+                                        self.advance()
+                                        return Token(TAKEOFF, "takeoff")
+                elif self.current_char == "r":
+                        ident += self.current_char
+                        self.advance()
+                        if self.current_char == "a":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "c":
+                                ident += self.current_char
+                                self.advance()
+                                if self.current_char == "e":
+                                    ident += self.current_char
+                                    self.advance()
+                                    return Token(TRACE, "trace") 
+                        elif self.current_char == "u":
+                            ident += self.current_char
+                            self.advance()
+                            if self.current_char == "e":
+                                ident += self.current_char
+                                self.advance()         
+                                return Token(TRUE, "true")              
             else:
                 if self.current_char.isdigit() == True:
                     ident += str(self.current_char)
