c0b79711b616e57fc3a32878bf881a1e67ca779b
Melissa <mrmmiranda15@gmail.com>
2024-02-03 13:40:10 +0800

    added NODES and PARSER class

--- Mel/lexer.py
+++ Mel/lexer.py
@@ -1367,7 +1367,39 @@ class Lexer:
     #         return errors
         
         
-       
+#NODES
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+    def __repr__(self) -> str:
+        return f'{self.tok}'
+    
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node) -> None:
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self. right_node = right_node
+
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+    
+
+#PARSER
+    
+class Parser:
+    def __init__(self, tokens) -> None:
+        self.tokens = tokens
+        self.tok_idx = 1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+    
   
 
 def run(text):
