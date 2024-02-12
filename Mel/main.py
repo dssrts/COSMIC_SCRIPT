db00fdc268f495606350f3dd345ec529fb01c20a
Melissa <mrmmiranda15@gmail.com>
2024-02-13 00:19:45 +0800

    added error back to main, so we can see lexer error

--- Mel/main.py
+++ Mel/main.py
@@ -6,6 +6,11 @@ while True:
     result, error = lexer.run('<stdin>', text)
     
     print(result)
+
+    #REMOVE THIS FOR THE PARSER KASI IBA NA YUNG ERRORS NG PARSER
+    for err in error:
+        print(error)
+        
     """ for item in result:
         print(item)
     for err in error:
