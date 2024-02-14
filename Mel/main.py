6efcec828cfa3aca72c5588f4addf2e60be166ba
Melissa <mrmmiranda15@gmail.com>
2024-02-14 13:05:47 +0800

    reverted main - done testing

--- Mel/main.py
+++ Mel/main.py
@@ -1,15 +1,25 @@
 import lexer
 
 while True:
-    # text = input('cosmic script > ')
-    # #kaya dalawa kasi dalawa nirererturn ng make_tokens()
-    # result, error = lexer.run('<stdin>', text)
-    # print("this is the main branch")
+    text = input('cosmic script > ')
+    #kaya dalawa kasi dalawa nirererturn ng make_tokens()
+    result, error = lexer.run('<stdin>', text)
     
-    for item in result:
+    #print(result)
+
+    #REMOVE THIS FOR THE PARSER KASI IBA NA YUNG ERRORS NG PARSER
+    """ for err in error:
+        print(error) """
+    
+    if error: 
+        print(error.as_string())
+    else: 
+        print(result)
+        
+    """ for item in result:
         print(item)
     for err in error:
-        print(err)
+        print(err) """
     '''
     if error:
         for err in error:
