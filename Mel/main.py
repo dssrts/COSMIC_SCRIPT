85bb636475eaf5b60edd85cd42fdfa13514c1c55
Melissa <mrmmiranda15@gmail.com>
2024-02-14 13:03:28 +0800

    commented lines 4 - 7 in main

--- Mel/main.py
+++ Mel/main.py
@@ -1,10 +1,10 @@
 import lexer
 
 while True:
-    text = input('cosmic script > ')
-    #kaya dalawa kasi dalawa nirererturn ng make_tokens()
-    result, error = lexer.run('<stdin>', text)
-    print("this is the main branch")
+    # text = input('cosmic script > ')
+    # #kaya dalawa kasi dalawa nirererturn ng make_tokens()
+    # result, error = lexer.run('<stdin>', text)
+    # print("this is the main branch")
     
     for item in result:
         print(item)
